5609a46107a50915483c99acb785a649376dbdd7
francescoagati1975 <francescoagati1975@gmail.com>
2015-11-12 02:59:18 +0100

    is refactoring

---
+++ Is.hx
@@ -0,0 +1,18 @@
+#if macro
+  import haxe.macro.Expr;
+#end
+class Is {
+
+    public macro  static function is_array(obj:ExprOf<Dynamic>) {
+      return macro untyped Array.isArray($obj);
+    }
+    public macro static function is_primitive(obj:ExprOf<Dynamic>) {
+      return macro untyped __js__('typeof {0} == "string" || typeof {0} == "number"',$obj);
+    }
+
+
+    public macro static function isUndef(obj:ExprOf<Dynamic>) { return macro untyped $obj == undefined; }
+    public macro static function isDef(obj:ExprOf<Dynamic>) { return macro untyped $obj != undefined; }
+
+
+}
