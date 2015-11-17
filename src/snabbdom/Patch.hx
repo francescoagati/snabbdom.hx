dc38033f36eef5e563ccdfa91d48d3249e57db77
francescoagati1975 <francescoagati1975@gmail.com>
2015-11-17 22:45:24 +0100

    create native wrappe

--- src/snabbdom/Patch.hx
+++ src/snabbdom/Patch.hx
@@ -10,6 +10,8 @@ using snabbdom.VirtualNodeDataTools;
 @:build(ClassicFor.build())
 class Patch {
 
+  static var native = native;
+
   static var rg = untyped  __js__('new RegExp({0},"g")',"\\.");
 
 
@@ -84,8 +86,8 @@ class Patch {
       var hash = hashIdx > 0 ? hashIdx : untyped sel.length;
       var dot = dotIdx > 0 ? dotIdx : untyped sel.length;
       var tag = hashIdx != -1 || dotIdx != -1 ? untyped sel.slice(0, Math.min(hash, dot)) : sel;
-      elm = vnode.elm = isDef(data) && isDef(i = data.ns) ? NativeWrapper.createElementNS(i, tag)
-                                                          : NativeWrapper.createElement(tag);
+      elm = vnode.elm = isDef(data) && isDef(i = data.ns) ? native.createElementNS(i, tag)
+                                                          : native.createElement(tag);
       if (hash < dot) elm.id = untyped sel.slice(hash + 1, dot);
       //if (dotIdx > 0) elm.className = untyped sel.slice(dot+1,0).replace('.', ' ');
       //var s = "\\.";
@@ -99,7 +101,7 @@ class Patch {
         }
       } else if (is_primitive(untyped vnode.text)) {
 
-        elm.appendChild(NativeWrapper.createTextElement(vnode.text));
+        elm.appendChild(native.createTextElement(vnode.text));
       }
 
       Hooks.create(emptyNode,vnode);
@@ -114,7 +116,7 @@ class Patch {
 
       }
     } else {
-      elm = vnode.elm = untyped NativeWrapper.createTextElement(vnode.text);
+      elm = vnode.elm = untyped native.createTextElement(vnode.text);
     }
     return vnode.elm;
   }
