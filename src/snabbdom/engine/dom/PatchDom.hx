6682e918c9c17051536abdd132a57774c96645ca
francescoagati1975 <francescoagati1975@gmail.com>
2015-11-17 23:15:11 +0100

    using namespace engine.dom

---
+++ src/snabbdom/engine/dom/PatchDom.hx
@@ -0,0 +1,16 @@
+package snabbdom.engine.dom;
+
+import snabbdom.engine.dom.PatchDom.*;
+import snabbdom.Is.*;
+import snabbdom.engine.dom.plugins.Hooks;
+import snabbdom.engine.dom.plugins.NativeWrapper;
+using snabbdom.VirtualNodeDataTools;
+
+
+@:build(ClassicFor.build())
+@:partials(snabbdom.PatchTraits)
+class PatchDom implements partials.Partial {
+
+
+
+}
