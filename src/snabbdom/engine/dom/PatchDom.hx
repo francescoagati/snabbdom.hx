0624c28030bb570ad73f54412d0c49e07df8bf7f
francescoagati1975 <francescoagati1975@gmail.com>
2015-11-19 03:07:23 +0100

    change VirtualnNode in Vdom ref

--- src/snabbdom/engine/dom/PatchDom.hx
+++ src/snabbdom/engine/dom/PatchDom.hx
@@ -1,6 +1,7 @@
 package snabbdom.engine.dom;
 import snabbdom.engine.dom.VirtualNodesDom as Vnodes;
 import snabbdom.engine.dom.VirtualNodeDom as Vnode;
+  
 import snabbdom.engine.dom.PatchDom.*;
 import snabbdom.Is.*;
 import snabbdom.engine.dom.plugins.Hooks;
